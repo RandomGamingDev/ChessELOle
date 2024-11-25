a570cc84fc9b7a08ecf7e6298e58fc7ee39145e4
randomgamingdev <randomgamingdev@randomgamingdev.randomgamingdev>
2024-11-25 10:48:15 -0500

    added clarification

--- src/shared/get-pgn.tsx
+++ src/shared/get-pgn.tsx
@@ -2,14 +2,17 @@ import { Dispatch, MutableRefObject, SetStateAction } from "react";
 
 import LichessPgnViewer from 'lichess-pgn-viewer';
 
+// The API for exporting games is documented here: https://lichess.org/api#tag/Games/operation/gamePgn
+// https://lichess.org/${gameId} to access the game itself
 export const getPgn = (gameId: string, boardRef: MutableRefObject<HTMLElement | null>, setPgn: Dispatch<SetStateAction<string>>) => {
 	setPgn(""); // Set it to empty so that other components can tell that it's loading
-	fetch(`https://lichess.org/game/export/${gameId}?literate=1`)
+	fetch(`https://lichess.org/game/export/${gameId}?clocks=1`)
 		.then(res => res.text())
 		.then(txt => {
 			if (!boardRef.current)
 				return;
 
+			console.log(txt);
 			window.history.pushState(null, '', `#${gameId}`);
 			setPgn(txt);
 			const dispPgn = txt.split('\n').find(line => line.trim().startsWith("1. "));
