d52d083e1e31bed7bdca765e2e8ecb471cd71c4e
RandomGamingDev <randomgamingdev@gmail.com>
2024-11-25 18:38:55 -0500

    basic cleanup

--- src/components/details.tsx
+++ src/components/details.tsx
@@ -2,7 +2,7 @@ import { getPgnHeaderAttrib } from "@/shared/get-pgn-header-attrib";
 
 export default function Details({ pgn } : { pgn: string }) {
   return (
-		<div className="mx-8 dark:text-neutral-300 text-neutral-700 text-right right-0">
+		<div className="mx-8 dark:text-neutral-300 text-neutral-700 text-right float-right">
 			<h1 className="text-4xl font-bold pb-4">Game Details:</h1>
 			{
 				["Event", "Result", "Variant", "TimeControl", "ECO", "Opening", "Termination"].map(h => 
