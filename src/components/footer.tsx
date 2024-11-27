f460ab9d02b77fe8a550c10e445ee6fc19be8d2c
RandomGamingDev <randomgamingdev@gmail.com>
2024-11-27 15:58:44 -0500

    footer now changes color based on whether dark or light mode is used

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<div id="footer" className="text-neutral-300 fixed bottom-0 w-full">
+		<div id="footer" className="dark:text-neutral-300 text-neutral-500 fixed bottom-0 w-full">
 			<p className="text-center">
 				©2024 Created by <a className="underline" href="https://github.com/RandomGamingDev">RandomGamingDev</a> under the <a className="underline" href="https://github.com/RandomGamingDev/ChessELOle/blob/main/LICENSE">GPL-3</a> License with all code freely available <a className="underline" href="https://github.com/RandomGamingDev/ChessELOle">here</a>
 			</p>
